997515b12a5c6350c440726a8875f4e821e283da
hectorgarciatw <dreamallica@gmail.com>
2024-08-13 11:12:17 -0300

    Integrated the backend with the React build (exercise 3.11)

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
+app.use(express.static(path.join(__dirname, "dist")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
